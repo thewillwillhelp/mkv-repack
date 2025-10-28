9e4ccf6a22f2488d894ed44d3231485d31ea7359
thewillwillhelp <personalapp.team@gmail.com>
2025-10-28 02:43:38 +0100

    feat: working prototype

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 import logging
 import json
 from ve_utils.selection import SelectionMode
-from ve_utils.stream_info import StreamInfo, VideoContainer
+from ve_utils.stream_info import VideoContainer
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -37,11 +37,43 @@ def get_streams(file_path):
             check=True,
         )
 
-        logging.error(f"This ffprobe result: {result.stdout.strip()}")
+        logging.error("This ffprobe result: %s", result.stdout.strip())
         return result.stdout.strip()
     except (subprocess.CalledProcessError, FileNotFoundError) as exc:
-        logging.error(f"This is an error {str(exc)}")
+        logging.error("This is an error %s", str(exc))
         return str(exc)
+
+
+def update_streams(inp_file, out_file, streams_remapping):
+    try:
+        # streams_remapping = [
+        #     "-map", "0:v:0",
+        #     "-map", "0:a:1",
+        #     "-map", "0:s:3",
+        #     "-map", "0:s:4",
+        #     "-map", "0:s:1",
+        # ]
+        # logging.error("Update streams ffmpeg result: %s", streams_remapping)
+
+        result = subprocess.run(
+            [
+                "ffmpeg", "-i", inp_file,
+                *streams_remapping,
+                "-c", "copy",
+                "-disposition:s:0", "0",
+                out_file,
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        logging.error("Update streams ffmpeg result: %s", result.stdout.strip())
+        return result.stdout.strip()
+    except (subprocess.CalledProcessError, FileNotFoundError) as exc:
+        logging.error("Update streams error %s", str(exc))
+        return str(exc)
+
+
 
 
 def split_by_width(text, width):
@@ -86,15 +118,31 @@ def wrapped_main(stdscr):
     except KeyboardInterrupt as exc:
         raise exc
     except Exception as exc:
-        logging.error(f"Global error {str(exc)}")
+        logging.error("Global error %s", str(exc))
 
 
-def decorate_with_checbox(list_to_decorate):
-    result_list = []
-    for i, val in enumerate(list_to_decorate):
-        result_list.append(" [*] " + val)
-    return result_list
+def show_loading_status(stdscr, show=True):
+    height, width = stdscr.getmaxyx()
+    loading_text = "...processing"
+    if not show:
+        loading_text = len("...processing") * " "
 
+    stdscr.addstr(
+        height - 1, 0, loading_text,
+    )
+    stdscr.refresh()
+
+def parse_video(file_path):
+    if not file_path.endswith(".mkv"):
+        return VideoContainer()
+
+    streams_json = get_streams(file_path)
+    streams_data = json.loads(streams_json)
+
+    video_container = VideoContainer()
+    video_container.add_streams(streams_data)
+
+    return video_container
 
 
 def main(stdscr):
@@ -128,28 +176,51 @@ def main(stdscr):
         draw_list(stdscr, dir_content, selection.get_index("file_list"))
         draw_list(stdscr, video_container.get_stream_titles(), selection.get_index("stream_info"), left_offset=width // 2)
 
+        new_file_name = "tmp"
+        if selection.mode == "stream_info":
+            file_name = dir_content[selection.get_index("file_list")]
+            new_file_name = file_name.split(".")
+            new_file_name.insert(-1, "_2")
+            # new_file_name.append("_2")
+            new_file_name = ".".join(new_file_name)
+
+            stdscr.addstr(
+                height - 2, 0, f"Save update (S): {new_file_name}",
+            )
+
         key = stdscr.getch()
 
         if key == curses.KEY_UP:
             selection.selection_up()
+            video_container.move_dragged("up")
+
+            if selection.mode == "file_list":
+                selected_path = os.path.join(current_path, dir_content[selection.get_index("file_list")])
+                video_container = parse_video(selected_path)
         elif key == curses.KEY_DOWN:
             selection.selection_down()
+            video_container.move_dragged("down")
+
+            if selection.mode == "file_list":
+                selected_path = os.path.join(current_path, dir_content[selection.get_index("file_list")])
+                video_container = parse_video(selected_path)
         elif key == curses.KEY_RIGHT or key == ord("\n"):
-            selected_path = os.path.join(current_path, dir_content[selection.get_index("file_list")])
-            if os.path.isdir(selected_path):
-                current_path = selected_path
-                selection.set_index("file_list", 0)
-            else:
-                streams_json = get_streams(selected_path)
-                streams_data = json.loads(streams_json)
-                stdscr.addstr(
-                    height - 2, 0, f"Streams for {dir_content[selection.get_index("file_list")]}:"
-                )
+            if selection.mode == "file_list":
+                selected_path = os.path.join(current_path, dir_content[selection.get_index("file_list")])
+                if os.path.isdir(selected_path):
+                    current_path = selected_path
+                    selection.set_index("file_list", 0)
+                else:
+                    streams_json = get_streams(selected_path)
+                    streams_data = json.loads(streams_json)
 
-                video_container = VideoContainer()
-                video_container.add_streams(streams_data)
+                    video_container = VideoContainer()
+                    video_container.add_streams(streams_data)
 
-                selection.set_mode("stream_info")
+                    selection.set_mode("stream_info")
+            elif selection.mode == "stream_info":
+                video_container.toggle_dragged(selection.get_index("stream_info"))
+
         elif key == curses.KEY_LEFT:
             if selection.mode == "file_list":
                 current_path = os.path.dirname(current_path)
@@ -159,6 +230,20 @@ def main(stdscr):
         elif key == ord(" "):
             if selection.mode == "stream_info":
                 video_container.toggle_stream(selection.get_index("stream_info"))
+        elif key == ord("s"):
+            if selection.mode != "stream_info":
+                continue
+
+            logging.warning("Output format: ")
+            # for stream in video_container.streams:
+            #     if stream["checked"]:
+            #         logging.warning(str(stream["info"]))
+            # logging.warning(" ".join(video_container.get_streams_remapping()))
+            selected_path = os.path.join(current_path, dir_content[selection.get_index("file_list")])
+            show_loading_status(stdscr)
+            update_streams(selected_path, new_file_name, video_container.get_streams_remapping())
+            # show_loading_status(stdscr)
+
         elif key == ord("q"):
             break
 
--- ve_utils/stream_info.py
+++ ve_utils/stream_info.py
@@ -1,25 +1,82 @@
+import logging
+
 class VideoContainer:
     def __init__(self):
         self.streams = []
 
+    def get_next_stream_relative_index(self, stream_info):
+        stream_type = stream_info["codec_type"]
+        counter = 0
+
+        for i, stream in enumerate(self.streams):
+            if stream["info"].container["codec_type"] == stream_type:
+                counter += 1
+
+        return counter
+
+
     def add_streams(self, video_data):
         for stream in video_data["streams"]:
             stream_info = StreamInfo(stream)
+            stream_info.container["relative_index"] = self.get_next_stream_relative_index(stream_info.container)
             self.streams.append({
                 "info": stream_info,
-                "checked": True
+                "checked": True,
+                "dragged": False
             })
 
     def get_stream_titles(self):
         result_list = []
         for i, stream in enumerate(self.streams):
+            dragged_status = "> " if stream["dragged"] else "  "
             checked_status = "[X] " if stream["checked"] else "[ ] "
-            result_list.append(checked_status + str(stream["info"]))
+            result_list.append(dragged_status + checked_status + str(stream["info"]))
 
         return result_list
 
     def toggle_stream(self, index):
         self.streams[index]["checked"] = not self.streams[index]["checked"]
+
+    def toggle_dragged(self, index):
+        self.streams[index]["dragged"] = not self.streams[index]["dragged"]
+
+    def move_dragged(self, direction):
+        dragged_stream = None
+        dragged_index = None
+        for i, stream in enumerate(self.streams):
+            if stream["dragged"]:
+                dragged_stream = stream
+                dragged_index = i
+                break
+
+        if dragged_stream is None or dragged_index is None:
+            return
+
+        if direction == "up" and dragged_index > 0:
+            self.streams[dragged_index] = self.streams[dragged_index - 1]
+            self.streams[dragged_index - 1] = dragged_stream
+        elif direction == "down" and dragged_index < len(self.streams) - 1:
+            self.streams[dragged_index] = self.streams[dragged_index + 1]
+            self.streams[dragged_index + 1] = dragged_stream
+
+    def get_streams_remapping(self):
+        remapping_list = []
+        for stream in self.streams:
+            if not stream["checked"]:
+                continue
+            remapping_list.append("-map")
+            if stream["info"].container["codec_type"] == "video":
+                remapping_list.append(f"0:v:{stream['info'].container['relative_index']}")
+
+            if stream["info"].container["codec_type"] == "audio":
+                remapping_list.append(f"0:a:{stream['info'].container['relative_index']}")
+
+            if stream["info"].container["codec_type"] == "subtitle":
+                remapping_list.append(f"0:s:{stream['info'].container['relative_index']}")
+
+        return remapping_list
+
+
 
 
 class StreamInfo:
@@ -28,7 +85,7 @@ class StreamInfo:
             "index": stream_data["index"],
             "codec_type": stream_data["codec_type"],
             "codec_name": stream_data["codec_name"],
-
+            "relative_index": None
         }
 
         if "tags" in stream_data:
@@ -40,5 +97,8 @@ class StreamInfo:
 
     def __str__(self):
         stream_info = self.container
-        return (f"Stream {stream_info['index']} ({stream_info["codec_type"]}={stream_info['codec_name']}): " +
-            f"{stream_info["tags"].get("title", '')} ({stream_info["tags"].get("language", '')})")
+        return (f"Stream {stream_info['index']}" +
+            f"({stream_info["codec_type"]}{stream_info.get("relative_index", '-1')}={stream_info['codec_name']}): " +
+            f"{stream_info["tags"].get("title", '')} ({stream_info["tags"].get("language", '')})" +
+            f" - {stream_info["tags"].get("filename", '')}" +
+            f" - {stream_info["tags"].get("DURATION", '')}")
